3192e0910082529ab6e433e8206503f25a848ff6
0belous <47227283+0belous@users.noreply.github.com>
2024-04-17 20:10:15 +0100

    Organise mods into folders

---
+++ DevTools/Plugin.cs
@@ -0,0 +1,68 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Numerics;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+namespace DevTools
+{
+    [HarmonyPatch]
+    public class Patches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(GameSession), nameof(GameSession.RandomBagLevel))]
+        public static void Fixes(GameSession __instance, ref byte ___currentLevel, ref int __result)
+        {
+                    int mapId = Plugin.MapId;
+                    ___currentLevel = (byte)mapId;
+                    __result = ___currentLevel;
+                    Debug.Log("setting mapid to " + mapId);
+        }
+
+    }
+
+    [BepInPlugin("com.Obelous.DevTools", "DevTools", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public static int MapId = 0;
+        private void Awake()
+        {
+            Harmony harmony = new("com.Obelous.DevTools");
+            harmony.PatchAll();
+        }
+
+        private void Update()
+        {
+
+            if (SceneManager.GetActiveScene().name == "MainMenu")
+            {
+                SceneManager.LoadScene("CharacterSelect");
+            }
+
+            if (SceneManager.GetActiveScene().name.Contains("Level"))
+            {
+                if (Keyboard.current[Key.F1].wasPressedThisFrame)
+                {
+                    MapId = 0;
+                }
+                if (Keyboard.current[Key.F2].wasPressedThisFrame)
+                {
+                    MapId = 21;
+                }
+                if (Keyboard.current[Key.F3].wasPressedThisFrame)
+                {
+                    MapId = 37;
+                }
+                if (Keyboard.current[Key.F4].wasPressedThisFrame)
+                {
+                    MapId = 50;
+                }
+            }
+        }
+     }
+}
---
+++ RemoteExplosives/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BoplFixedMath;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace ModName
+{
+    [BepInPlugin("com.Obelous.RemoteExplosives", "Remote Explosives", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo("Plugin Remote Explosives is loaded!");
+
+            Harmony harmony = new Harmony("com.Obelous.RemoteExplosives");
+
+            MethodInfo engine = AccessTools.Method(typeof(RocketEngine), "Awake");
+            MethodInfo enginePatched = AccessTools.Method(typeof(enginePatches), "patch");
+            harmony.Patch(engine, new HarmonyMethod(enginePatched));
+
+        }
+
+        public class enginePatches
+        {
+            public static void patch(RocketEngine __instance)
+            {
+                __instance.radius = (Fix)100f;
+            }
+        }
+    }
+}
